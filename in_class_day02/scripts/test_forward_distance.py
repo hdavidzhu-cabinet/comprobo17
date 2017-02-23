eba2e76e7e27b024339f7ae134356a16c588a63e
hdavidzhu <hdavidzhu@gmail.com>
2017-02-23 15:33:03 -0500

    Committed private documents

---
+++ in_class_day02/scripts/test_forward_distance.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from geometry_msgs.msg import PointStamped, Point
+from std_msgs.msg import Header
+from sensor_msgs.msg import LaserScan
+
+import rospy
+
+def process_scan(msg):
+    print msg.ranges[0]
+
+rospy.init_node('forward_distance')
+rospy.Subscriber('/scan', LaserScan, process_scan)
+
+r = rospy.Rate(2)
+while not rospy.is_shutdown():
+    r.sleep()
+
+print "Node is finished!"
--- in_class_day02_solution/scripts/test_message.py
+++ in_class_day02_solution/scripts/test_message.py
@@ -14,7 +14,7 @@ publisher = rospy.Publisher('/my_topic', PointStamped, queue_size=10)
 
 r = rospy.Rate(2)
 while not rospy.is_shutdown():
-    print "looping", loop_count
+    # print "looping", loop_count
     loop_count += 1
     my_header = Header(loop_count, rospy.Time.now(), "odom")
     my_point = Point(x=3.2, y=5.4)
